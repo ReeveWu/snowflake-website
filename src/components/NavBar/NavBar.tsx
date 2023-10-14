ad4c604cc49afb2269edc47f460b00e6b1ede026
ReeveWu <reeve0319@gmail.com>
2023-10-14 14:02:21 +0800

    update: root router

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,17 +7,17 @@ const NavBar = () => {
   return (
     <>
       <nav className="navbar">
-        <Link to="">
+        <Link to="./">
           <h1>成年璃。從心開始</h1>
         </Link>
         <div className="links mr-10">
-          <Link to="">
+          <Link to=".">
             <button className="btn nav-component cbtn btn-primary">首頁</button>
           </Link>
-          <Link to="">
+          <Link to="./">
             <span className="nav-component">活動簡介</span>
           </Link>
-          <Link to="">
+          <Link to="./">
             <span className="nav-component">線上活動</span>
           </Link>
         </div>
