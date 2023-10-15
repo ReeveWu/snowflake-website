a19ba4786af5099bed306b3e496e10c7fcfa470d
ReeveWu <reeve0319@gmail.com>
2023-10-15 15:08:52 +0800

    update: router mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import logo from "./assets/logo.jpg";
 import NavBar from "./components/NavBar/NavBar";
