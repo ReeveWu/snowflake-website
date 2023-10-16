82e552b5b4ba34c2e8445384a98825e2c811ba24
ReeveWu <reeve0319@gmail.com>
2023-10-16 16:11:03 +0800

    update: navbar css

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
 import "./style.css";
-// import "bootstrap/dist/css/bootstrap.css";
 
 const NavBar = () => {
+  const [currentPage, setCurrentPage] = useState(0);
   return (
     <nav className="navbar">
       <Link to="/">
@@ -11,13 +12,41 @@ const NavBar = () => {
       </Link>
       <div className="links">
         <Link to="/">
-          <button className="btn nav-component cbtn btn-primary">首頁</button>
+          <div
+            className="nav-component"
+            onClick={() => setCurrentPage(0)}
+            style={{
+              borderBottom: currentPage === 0 ? "5px solid #38a3a5" : "none",
+            }}
+          >
+            <HomeIcon
+              style={{
+                fontSize: "32px",
+              }}
+            />
+          </div>
         </Link>
         <Link to="/about">
-          <span className="nav-component">活動簡介</span>
+          <div
+            className="nav-component"
+            onClick={() => setCurrentPage(1)}
+            style={{
+              borderBottom: currentPage === 1 ? "5px solid #38a3a5" : "none",
+            }}
+          >
+            活動簡介
+          </div>
         </Link>
         <Link to="/posts">
-          <span className="nav-component">玻璃心留言板</span>
+          <div
+            className="nav-component"
+            onClick={() => setCurrentPage(2)}
+            style={{
+              borderBottom: currentPage === 2 ? "5px solid #38a3a5" : "none",
+            }}
+          >
+            玻璃心留言板
+          </div>
         </Link>
       </div>
     </nav>
